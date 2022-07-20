156ee1496f2729d6776064d97b3dccfa998e8181
1_1_LJS <jeongseob.korea@gmail.com>
2022-07-20 16:32:40 +0900

    finished 3

--- Practices/Python_Practices/SW_Expert_Academy/2019.py
+++ Practices/Python_Practices/SW_Expert_Academy/2019.py
@@ -1,8 +1,3 @@
-import sys
-sys.stdin = open('2019_input.txt')
-
 N = int(input())
-answer = []
 for number in range(N + 1):
-    answer.append(2 ** number)
-print(answer)
+    print((2**number), end = ' ')
--- Practices/Python_Practices/SW_Expert_Academy/2050.py
+++ Practices/Python_Practices/SW_Expert_Academy/2050.py
@@ -4,4 +4,4 @@ sys.stdin = open('2050_input.txt')
 s = input()
 result = 0
 for i in s:
-    print(ord(i)-64, end='')
+    print(ord(i)-64, end=' ')

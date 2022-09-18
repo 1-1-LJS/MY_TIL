6413b29d2dd5b64525b0642554c2bb2eb8b58844
1_1_LJS <jeongseob.korea@gmail.com>
2022-09-18 22:20:14 +0900

    codeup.0918

---
+++ Practices/Python_Practices/Basic_Python_100/6056.py
@@ -0,0 +1,5 @@
+﻿a, b=input().split()
+c = bool(int(a))
+d = bool(int(b))
+print((c and (not d)) or ((not c) and d))
+
---
+++ Practices/Python_Practices/Basic_Python_100/6058.py
@@ -0,0 +1,16 @@
+﻿a, b = input().split()
+c= bool(int(a))
+d= bool(int(b))
+
+print( not (c or d) )
+
+
+# Alt 1
+# a, b = map(int, input().split())
+# a, b = bool(a), bool(b)
+
+# if (a == False and b == False):
+#     print("True")
+# else:
+#     print("False")
+    

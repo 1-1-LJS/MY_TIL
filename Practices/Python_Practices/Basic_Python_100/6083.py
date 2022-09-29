357a56ebd8cf9847a3dc79f0b31dbe926fca0597
1_1_LJS <jeongseob.korea@gmail.com>
2022-09-29 20:34:37 +0900

    almost

---
+++ Practices/Python_Practices/Basic_Python_100/6083.py
@@ -0,0 +1,27 @@
+﻿r, g, b = map(int,input().split())
+
+count=0
+
+for i in range(r):
+    for j in range(g):
+        for k in range(b):
+            print('%d %d %d' %(i,j,k))
+            
+            count = count + 1
+            
+print(count)
+
+
+
+# r, g, b = input().split()
+
+# r = int(r)
+# g = int(g)
+# b = int(b)
+
+# for i in range(0, r) :
+#   for j in range(0, g) :
+#     for k in range(0, b) :
+#       print(i, j, k)
+
+# print(r*g*b)
